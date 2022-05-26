3df1c56a487d8b9b4cd8c39cda364e199d31abd4
viordache <iordacheviorel99@gmail.com>
2022-05-26 17:14:03 +0300

    prove termiantion for the first 6 rules

--- cnf.scala
+++ cnf.scala
@@ -20,12 +20,12 @@ package object cnf {
 
     // def applyRule1
     
-    def applyRule1(f : FormulaT, orsAboveLeft : BigInt, andsAboveLef : BigInt) : FormulaT = {
+    def applyRule1(f : FormulaT, orsAboveLeft : BigInt, andsAboveLeft : BigInt) : FormulaT = {
 
         require(formula.validFormulaT(f))
         require(f.isInstanceOf[DImplies])
-        // require(orsAboveLeft >= 0)
-        // require(andsAboveLeft >= 0)
+        require(orsAboveLeft >= 0)
+        require(andsAboveLeft >= 0)
         
         val DImplies(f1, f2) = f
         val result = And(Implies(f1, f2), Implies(f2, f1))
@@ -41,8 +41,113 @@ package object cnf {
 
     } ensuring(res =>
         (validFormulaT(res))
-        &&
-        (countDImplies(res) < countDImplies(f))
+        && (weightOfAnds(res) <= weightOfAnds(f))
+        && (countDImplies(res) < countDImplies(f))
+    )
+
+    def applyRule2(f : FormulaT, orsAboveLeft : BigInt, andsAboveLeft : BigInt) : FormulaT = {
+
+        require(formula.validFormulaT(f))
+        require(f.isInstanceOf[Implies])
+        require(orsAboveLeft >= 0)
+        require(andsAboveLeft >= 0)
+        
+        val Implies(f1, f2) = f
+        val result = Or(Not(f1), f2)
+        
+        assert(weightOfAnds(result) <= weightOfAnds(f))
+
+        result
+    } ensuring(res =>
+        (validFormulaT(res))
+        && (weightOfAnds(res) <= weightOfAnds(f))
+        && (countDImplies(res) <= countDImplies(f))
+        && (countImplies(res) < countImplies(f))
+    )
+
+    def applyRule3(f : FormulaT, orsAboveLeft : BigInt, andsAboveLeft : BigInt) : FormulaT = {
+
+        require(formula.validFormulaT(f))
+        require(f.isInstanceOf[Or])
+        require(orsAboveLeft >= 0)
+        require(andsAboveLeft >= 0)
+        
+        val Or(f1, f2) = f
+        require(f2.isInstanceOf[And])
+        val And(f21, f22) = f2
+        val result = And(Or(f1, f21), Or(f1, f22))
+
+        result
+    } ensuring(res =>
+        (validFormulaT(res))
+        && (weightOfAnds(res) < weightOfAnds(f))
+    )
+
+    def applyRule4(f : FormulaT, orsAboveLeft : BigInt, andsAboveLeft : BigInt) : FormulaT = {
+
+        require(formula.validFormulaT(f))
+        require(f.isInstanceOf[Or])
+        require(orsAboveLeft >= 0)
+        require(andsAboveLeft >= 0)
+        
+        val Or(f1, f2) = f
+        require(f1.isInstanceOf[And])
+        val And(f11, f12) = f1
+        val result = And(Or(f11, f2), Or(f12, f2))
+
+        result
+    } ensuring(res =>
+        (validFormulaT(res))
+        && (weightOfAnds(res) < weightOfAnds(f))
+    )
+
+    def applyRule5(f : FormulaT, orsAboveLeft : BigInt, andsAboveLeft : BigInt) : FormulaT = {
+
+        require(formula.validFormulaT(f))
+        require(f.isInstanceOf[Or])
+        require(orsAboveLeft >= 0)
+        require(andsAboveLeft >= 0)
+        
+        val Or(f1, f2) = f
+        require(f2.isInstanceOf[Or])
+        val Or(f21, f22) = f2
+        val result = Or(Or(f1, f21), f22)
+        
+        Rule5Prop(f1, f21, f22, orsAboveLeft)
+        assert (countOrPairs(result, orsAboveLeft) < countOrPairs(f, orsAboveLeft))
+
+        result
+    } ensuring(res =>
+        (validFormulaT(res))
+        && (weightOfAnds(res) <= weightOfAnds(f))
+        && (countDImplies(res) <= countDImplies(f))
+        && (countImplies(res) <= countImplies(f))
+        && (countOrPairs(res, orsAboveLeft) < countOrPairs(f, orsAboveLeft))
+    )
+
+    def applyRule6(f : FormulaT, orsAboveLeft : BigInt, andsAboveLeft : BigInt) : FormulaT = {
+
+        require(formula.validFormulaT(f))
+        require(f.isInstanceOf[And])
+        require(orsAboveLeft >= 0)
+        require(andsAboveLeft >= 0)
+        
+        val And(f1, f2) = f
+        require(f2.isInstanceOf[And])
+        val And(f21, f22) = f2
+        val result = And(And(f1, f21), f22)
+        
+        Rule6Prop(f1, f21, f22, andsAboveLeft)
+        assert (countAndPairs(result, andsAboveLeft) < countAndPairs(f, andsAboveLeft))
+
+        result
+    } ensuring(res =>
+        (validFormulaT(res))
+        && (weightOfAnds(res) <= weightOfAnds(f))
+        && (countDImplies(res) <= countDImplies(f))
+        && (countImplies(res) <= countImplies(f))
+        && (countOrPairs(res, orsAboveLeft) <= countOrPairs(f, orsAboveLeft))
+         && (countAndPairs(res, andsAboveLeft) < countAndPairs(f, andsAboveLeft))
     )
 
 
@@ -50,7 +155,23 @@ package object cnf {
     ///// FUNCTIONS /////
     /////////////////////
 
-    @induct
+    // @induct // we receive termination errors when trying to induct over this method
+    def weightOfAnds(f : FormulaT) : BigInt = {
+        decreases(f)
+        f match {
+            case Var(v)           => BigInt(2)
+            case Not(f1)          => pow(2, weightOfAnds(f1))
+            case Or(f1, f2)       => weightOfAnds(f1) * weightOfAnds(f2)
+            case And(f1, f2)      => weightOfAnds(f1) + weightOfAnds(f2) + BigInt(2)
+            case Implies(f1, f2)  => pow(BigInt(2), weightOfAnds(f1)) * weightOfAnds(f2)
+            case DImplies(f1, f2) => pow(BigInt(2), weightOfAnds(f1)) * weightOfAnds(f2) +
+                pow(BigInt(2), weightOfAnds(f2)) * weightOfAnds(f1) + BigInt(2)
+        }
+    } ensuring(
+        res => res >= 2
+    )
+
+    // @induct // we don't receive errors on induction but the verification doesn't seem to need this
     def countDImplies(f: FormulaT) : BigInt = {
 
         f match {
@@ -65,10 +186,159 @@ package object cnf {
         res => res >= 0
     )
 
+    def countImplies(f: FormulaT) : BigInt = {
+
+        f match {
+            case Var(v)           => BigInt(0)
+            case Not(f1)          => countImplies(f1)
+            case Or(f1, f2)       => countImplies(f1) + countImplies(f2)
+            case And(f1, f2)      => countImplies(f1) + countImplies(f2)
+            case Implies(f1, f2)  => countImplies(f1) + countImplies(f2) + 1
+            case DImplies(f1, f2) => countImplies(f1) + countImplies(f2)
+        }
+    } ensuring(
+        res => res >= 0
+    )
+
+    // @induct
+    def countOrPairs(f: FormulaT, orsAboveLeft : BigInt) : BigInt = {
+        require(orsAboveLeft >= 0)
+
+        f match {
+            case Var(v)           => BigInt(0)
+            case Not(f1)          => countOrPairs(f1, orsAboveLeft)
+            case Or(f1, f2)       => countOrPairs(f1, orsAboveLeft) + countOrPairs(f2, orsAboveLeft + 1) + orsAboveLeft
+            case And(f1, f2)      => countOrPairs(f1, orsAboveLeft) + countOrPairs(f2, orsAboveLeft)
+            case Implies(f1, f2)  => countOrPairs(f1, orsAboveLeft) + countOrPairs(f2, orsAboveLeft)
+            case DImplies(f1, f2) => countOrPairs(f1, orsAboveLeft) + countOrPairs(f2, orsAboveLeft)
+        }
+    } ensuring(
+        res => res >= 0
+    )
+
+    def countAndPairs(f: FormulaT, andsAboveLeft : BigInt) : BigInt = {
+        require(andsAboveLeft >= 0)
+
+        f match {
+            case Var(v)           => BigInt(0)
+            case Not(f1)          => countAndPairs(f1, andsAboveLeft)
+            case Or(f1, f2)       => countAndPairs(f1, andsAboveLeft) + countAndPairs(f2, andsAboveLeft)
+            case And(f1, f2)      => countAndPairs(f1, andsAboveLeft) + countAndPairs(f2, andsAboveLeft + 1) + andsAboveLeft
+            case Implies(f1, f2)  => countAndPairs(f1, andsAboveLeft) + countAndPairs(f2, andsAboveLeft)
+            case DImplies(f1, f2) => countAndPairs(f1, andsAboveLeft) + countAndPairs(f2, andsAboveLeft)
+        }
+    } ensuring(
+        res => res >= 0
+    )
+
+    /////////////////////
+    ///// LEMMAS ////////
+    /////////////////////
+
     @induct
     def mult2_upper(x : BigInt) : Boolean = {
 
-        require(x >= BigInt(0))
-        BigInt(2) * x < BigInt(1) + pow(BigInt(2), x)
+        require(x >= 0)
+        2 * x < 1+ pow(2, x)
     }.holds
+
+    // def Rule5Prop(f1 : FormulaT, f2 : FormulaT, f3 : FormulaT, orsAboveLeft : BigInt) : Boolean = {
+
+    //     require(orsAboveLeft >= 0)
+
+    //     countOrPairs(Or(f1, Or(f2, f3)), orsAboveLeft) ==
+    //         countOrPairs(f1, orsAboveLeft) + countOrPairs(f2, orsAboveLeft + 1) +
+    //         countOrPairs(f3, orsAboveLeft + 2) + orsAboveLeft + orsAboveLeft + 1
+    //     countOrPairs(Or(Or(f1, f2), f3), orsAboveLeft) ==
+    //         countOrPairs(f1, orsAboveLeft) +
+    //         countOrPairs(f2, orsAboveLeft + 1) +
+    //         countOrPairs(f3, orsAboveLeft + 1) + orsAboveLeft + orsAboveLeft
+    //     Rule5PropAux(f3, orsAboveLeft + 1)
+    //     // countOrPairs(f3, orsAboveLeft +1) <= countOrPairs(f3, orsAboveLeft + 2)
+    //     countOrPairs(f3, orsAboveLeft + 1) <= countOrPairs(f3, orsAboveLeft + 2)
+    //     countOrPairs(Or(Or(f1, f2), f3), orsAboveLeft) <
+    //         countOrPairs(Or(f1, Or(f2, f3)), orsAboveLeft)
+    // }.holds
+
+    // def Rule5PropAux(f : FormulaT, orsAboveLeft : BigInt) : Unit = {
+    //     require(orsAboveLeft >= 0)
+
+  
+    //     assert(countOrPairs(f, orsAboveLeft) <= countOrPairs(f, orsAboveLeft + 1))
+    // }
+
+    def Rule5PropAux2(f : FormulaT, orsAboveLeft : BigInt) : Boolean = {
+        require (orsAboveLeft >= 0)
+
+        (countOrPairs(f, orsAboveLeft) <= countOrPairs(f, orsAboveLeft + 1)) because {
+            f match {
+                case Var(v)           => true
+                case Not(f1)          => Rule5PropAux2(f1, orsAboveLeft)
+                case Or(f1, f2)       => Rule5PropAux2(f1, orsAboveLeft) && Rule5PropAux2(f2, orsAboveLeft + 1)
+                case And(f1, f2)      => Rule5PropAux2(f1, orsAboveLeft) && Rule5PropAux2(f2, orsAboveLeft)
+                case Implies(f1, f2)  => Rule5PropAux2(f1, orsAboveLeft) && Rule5PropAux2(f2, orsAboveLeft)
+                case DImplies(f1, f2) => Rule5PropAux2(f1, orsAboveLeft) && Rule5PropAux2(f2, orsAboveLeft)
+            }
+        }   
+    }.holds
+
+    def foo(x: BigInt): Unit = {
+        check(x >= 0 || x < 0)
+        check(x + 0 == x)
+    }
+
+    
+    def Rule5Prop(f1 : FormulaT, f2 : FormulaT, f3 : FormulaT, orsAboveLeft : BigInt) : Unit = {
+
+        require(orsAboveLeft >= 0)
+
+        check(countOrPairs(Or(f1, Or(f2, f3)), orsAboveLeft) ==
+            countOrPairs(f1, orsAboveLeft) + 
+            countOrPairs(f2, orsAboveLeft + 1) +
+            countOrPairs(f3, orsAboveLeft + 2) + orsAboveLeft + orsAboveLeft + 1)
+        check(countOrPairs(Or(Or(f1, f2), f3), orsAboveLeft) ==
+            countOrPairs(f1, orsAboveLeft) +
+            countOrPairs(f2, orsAboveLeft + 1) +
+            countOrPairs(f3, orsAboveLeft + 1) + orsAboveLeft + orsAboveLeft)
+        Rule5PropAux2(f3, orsAboveLeft + 1)
+        check(countOrPairs(f3, orsAboveLeft +1) <= countOrPairs(f3, orsAboveLeft + 2))
+        // countOrPairs(f3, orsAboveLeft + 1) <= countOrPairs(f3, orsAboveLeft + 2)
+        check(countOrPairs(Or(Or(f1, f2), f3), orsAboveLeft) <
+            countOrPairs(Or(f1, Or(f2, f3)), orsAboveLeft))
+    }
+
+    def Rule6PropAux(f : FormulaT, andsAboveLeft : BigInt) : Boolean = {
+        require (andsAboveLeft >= 0)
+
+        (countAndPairs(f, andsAboveLeft) <= countAndPairs(f, andsAboveLeft + 1)) because {
+            f match {
+                case Var(v)           => true
+                case Not(f1)          => Rule6PropAux(f1, andsAboveLeft)
+                case Or(f1, f2)       => Rule6PropAux(f1, andsAboveLeft) && Rule6PropAux(f2, andsAboveLeft)
+                case And(f1, f2)      => Rule6PropAux(f1, andsAboveLeft) && Rule6PropAux(f2, andsAboveLeft + 1)
+                case Implies(f1, f2)  => Rule6PropAux(f1, andsAboveLeft) && Rule6PropAux(f2, andsAboveLeft)
+                case DImplies(f1, f2) => Rule6PropAux(f1, andsAboveLeft) && Rule6PropAux(f2, andsAboveLeft)
+            }
+        }   
+    }.holds
+
+    def Rule6Prop(f1 : FormulaT, f2 : FormulaT, f3 : FormulaT, andsAboveLeft : BigInt) : Unit = {
+
+        require(andsAboveLeft >= 0)
+
+        check (countAndPairs(And(f1, And(f2, f3)), andsAboveLeft) ==
+            countAndPairs(f1, andsAboveLeft) +
+            countAndPairs(f2, andsAboveLeft + 1) +
+            countAndPairs(f3, andsAboveLeft + 2) + andsAboveLeft + andsAboveLeft + 1)
+        check (countAndPairs(And(And(f1, f2), f3), andsAboveLeft) ==
+            countAndPairs(f1, andsAboveLeft) +
+            countAndPairs(f2, andsAboveLeft + 1) +
+            countAndPairs(f3, andsAboveLeft + 1) + andsAboveLeft + andsAboveLeft)
+        Rule6PropAux(f3, andsAboveLeft + 1)
+        check (countAndPairs(f3, andsAboveLeft + 1) <=
+            countAndPairs(f3, andsAboveLeft + 2))
+        check (countAndPairs(And(And(f1, f2), f3), andsAboveLeft) <
+            countAndPairs(And(f1, And(f2, f3)), andsAboveLeft))
+    }
+
 }
--- utils.scala
+++ utils.scala
@@ -4,6 +4,7 @@ import scala.language.postfixOps
 import stainless.annotation._
 import stainless.proof._ 
 import stainless.proof.check
+import stainless.lang._ 
 
 package object utils {
 
@@ -40,4 +41,97 @@ package object utils {
                 // x * exp 
             }
     } ensuring(res => res > 0)
+
+    /////////////////////
+    //// LEMMAS /////////
+    /////////////////////
+
+    // @induct // by default in dafny
+    def pow_monotone(a : BigInt, b : BigInt) : Boolean = {
+
+        require(0 <= a)
+        require(a <= b)
+
+        (pow(2, a) <= pow(2, b)) because {
+            if (a == b)  
+                pow(2, a) == pow(2, b) 
+            else
+                pow_monotone(a, b - 1)
+        }
+    }.holds
+
+    def pow_monotone_strict(a : BigInt, b : BigInt) : Boolean = {
+
+        require(0 <= a)
+        require(a < b)
+
+        (pow(2, a) < pow(2, b)) because {
+            if (a + 1 == b)  
+                pow(2, a) < pow(2, b) 
+            else
+                pow_monotone_strict(a, b - 1)
+        }
+    }.holds
+
+    def multpow_powsum(a : BigInt, b : BigInt) : Boolean = {
+
+        require(a >= 1)
+        require(b >= 1)
+
+        (pow(2, a) * pow(2, b) == pow(2, a + b)) because {
+            if (b > 2)  
+                multpow_powsum(a, b - 1) 
+            else
+                pow(2, a) * pow(2, 2) == pow(2, a + 2)
+        }
+    }.holds
+
+
+    def sumpow_upper(a : BigInt, b : BigInt) : Unit = {
+
+        require(a >= 1)
+        require(b >= 2)
+        require(a >= b)
+
+        pow_monotone(a * 2, a * b)
+        assert (pow(2, a * 2) <= pow(2, a * b))
+        assert (pow(2, a + a) <= pow(2, a * 2))
+        multpow_powsum(a, a)
+        assert (pow(2, a) * pow(2, a) <= pow(2, a + a))
+        
+        assert (4 <= pow(2, a))
+        assert (4 * pow(2, a) <= pow(2, a) * pow(2, a))
+        check (pow(2, a) + pow(2, a) + pow(2, a) + pow(2, a) <= 4 * pow(2, a))
+        pow_monotone(b, a)
+        check (pow(2, b) <= pow(2, a))
+        check (pow(2, a) + pow(2, b) + 1 <
+            pow(2, a) + pow(2, a) + pow(2, a) + pow(2, a))
+    }
+
+    def mult2_upper(x : BigInt) : Boolean = {
+
+        require(x >= 0)
+
+        (2 * x < 1 + pow(2, x)) because {
+            if (x == 0)  
+                true
+            else
+                mult2_upper(x - 1)
+        }
+    }
+
+    // def lessthan_mult_right(a : BigInt, b : BigInt, c : BigInt) : Unit = {
+    
+    //     require(a >= 1)
+    //     require(b >= 1)
+    //     require(c >= 1)
+    //     require(c < b)
+        
+    //     assert(a * c < a)
+
+    // }
+
+
+
+
 }
